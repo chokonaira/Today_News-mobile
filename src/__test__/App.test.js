2fa6adda1fa144a12c549c2689a64b74c8239bbd
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2021-02-10 19:50:15 +0200

    added a test case

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import { Text } from 'react-native';
 
 import App from '../../App';
 
 describe('<App />', () => {
   it('has a welcome text', () => {
     const tree = render(<App />).toJSON();
-    console.log(tree.children[0].children[0])
     expect(tree.children[0].children[0]).toBe('Welcome to Today News');
   });
 });
