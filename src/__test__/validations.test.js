f4dd1c6b742fb1581b966c37968612bd2cfa8443
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2021-02-17 22:49:24 +0200

    setup jest.config.js adds unit test for all validation scenrios refactor email validation regex

---
+++ src/__test__/validations.test.js
@@ -0,0 +1,88 @@
+import {
+  usernameValidation,
+  emailValidation,
+  passwordValidation,
+} from "../helpers/validations";
+
+describe("Input Validations", () => {
+  it("validates for an empty username", () => {
+    const validateUsername = usernameValidation("");
+    expect(validateUsername).toEqual("Username cannot be empty");
+  });
+
+  it("validates for an username length", () => {
+    const validateUsername = usernameValidation("hen");
+    expect(validateUsername).toEqual("Username must be more than 4 characters");
+  });
+
+  it("validates for an username type", () => {
+    const validateUsername = usernameValidation("1234");
+    expect(validateUsername).toEqual("Username cannot be only digits");
+  });
+
+  it("validates for a correct username of all letters", () => {
+    const validateUsername = usernameValidation("henry");
+    expect(validateUsername).toEqual(null);
+  });
+
+  it("validates for a correct username of letters & digits", () => {
+    const validateUsername = usernameValidation("henry2021");
+    expect(validateUsername).toEqual(null);
+  });
+
+  it("Validates username and trims white spaces", () => {
+    const validateUsername = usernameValidation("henry2021    ");
+    expect(validateUsername).toEqual(null);
+  });
+
+  it("validates for an empty Email", () => {
+    const validateEmail = emailValidation("");
+    expect(validateEmail).toEqual("Email cannot be empty");
+  });
+
+  it("validates for an wrong Emails format", () => {
+    let validate;
+    [
+      "henry",
+      "henrry@",
+      "henry@gmail",
+      "henry.com",
+      "@gmail.com",
+      ".com",
+    ].forEach((string) => {
+      validate = emailValidation(string);
+    });
+    expect(validate).toEqual("Email format is incorrect");
+  });
+
+  it("Validates correct Email format", () => {
+    const validateEmail = emailValidation("henry@gmail.com");
+    expect(validateEmail).toEqual(null);
+  });
+
+  it("Validates Email and trims white spaces", () => {
+    const validateEmail = emailValidation("henry@gmail.com    ");
+    expect(validateEmail).toEqual(null);
+  });
+
+  it("validates for an empty password", () => {
+    const validatePassword = passwordValidation("");
+    expect(validatePassword).toEqual("Password cannot be empty");
+  });
+
+  it("validates incorrect Password length", () => {
+    const validatePassword = passwordValidation("henry");
+    expect(validatePassword).toEqual("Password should be more than 6 characters");
+  });
+
+  it("validates correct Password length", () => {
+    const validatePassword = passwordValidation("okonkwo");
+    expect(validatePassword).toEqual(null);
+  });
+
+  it("Validates Password and trims white spaces", () => {
+    const validatePassword = passwordValidation("okonkwo    ");
+    expect(validatePassword).toEqual(null);
+  });
+
+});
--- src/helpers/validations.js
+++ src/helpers/validations.js
@@ -13,25 +13,25 @@ export const usernameValidation = (username) => {
   return message;
 };
 
-export const passwordValidation = (password) => {
-  let message = null;
-  let value = password.trim();
-  if (value === "") {
-    massage = "Password cannot be empty";
-  } else if (value.length < 6) {
-    message = "Password should be more than 6 characters";
-  }
-  return message;
-};
-
 export const emailValidation = (email) => {
   let message = null;
   let value = email.trim();
-  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (value === "") {
     message = "Email cannot be empty";
   } else if (!emailRegex.test(String(value).toLowerCase())) {
     message = "Email format is incorrect";
+  }
+  return message;
+};
+
+export const passwordValidation = (password) => {
+  let message = null;
+  let value = password.trim()
+  if (value === "") {
+    message = "Password cannot be empty";
+  } else if (value.length < 6) {
+    message = "Password should be more than 6 characters";
   }
   return message;
 };
