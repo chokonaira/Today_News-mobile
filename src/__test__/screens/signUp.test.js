85677298bd424c90cce166696f01c45ebf89dfc4
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2021-02-25 10:02:34 +0200

    added test for signup input errors on blur

--- src/__test__/screens/signUp.test.js
+++ src/__test__/screens/signUp.test.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { render, fireEvent, cleanup } from "react-native-testing-library";
-import SignIn from "../../screens/SignIn";
+import SignUp from "../../screens/SignUp";
 import Input from "../../components/Input";
+
+
 import configureStore from "redux-mock-store";
 import { Provider } from "react-redux";
 
 jest.mock("react-native-vector-icons/MaterialIcons", () => "Icon");
 
-describe("<SignIn />", () => {
+describe("<SignUp />", () => {
   afterEach(cleanup);
   const mockStore = configureStore([]);
   const initialState = {
@@ -16,11 +18,23 @@ describe("<SignIn />", () => {
     },
   };
   const store = mockStore(initialState);
+  it("shows an error message for empty username input", () => {
+    const tree = render(
+      <Provider store={store}>
+        <SignUp />
+      </Provider>
+    );
+
+    const usernameInput = tree.getByTestId("username");
+    fireEvent(usernameInput, "blur");
+    const errorElement = tree.getByTestId("errorText");
+    expect(errorElement.props.children).toEqual("Username cannot be empty");
+  });
 
   it("shows an error message for empty email input", () => {
     const tree = render(
       <Provider store={store}>
-        <SignIn />
+        <SignUp />
       </Provider>
     );
 
@@ -33,7 +47,7 @@ describe("<SignIn />", () => {
   it("shows an error message for empty password input", () => {
     const tree = render(
       <Provider store={store}>
-        <SignIn />
+        <SignUp />
       </Provider>
     );
 
@@ -46,7 +60,7 @@ describe("<SignIn />", () => {
   it("shows an error message when email is wrongly formatted", () => {
     const tree = render(
       <Provider store={store}>
-        <SignIn />
+        <SignUp />
       </Provider>
     );
     const emailInput = tree.getByTestId("email");
@@ -60,7 +74,7 @@ describe("<SignIn />", () => {
   it("shows no error component when email is rightly formatted", () => {
     const tree = render(
       <Provider store={store}>
-        <SignIn />
+        <SignUp />
       </Provider>
     );
 
@@ -77,10 +91,62 @@ describe("<SignIn />", () => {
     expect(errorElement.props.children).toEqual(undefined);
   });
 
+  it("shows an error message when username characters are less", () => {
+    const tree = render(
+      <Provider store={store}>
+        <SignUp />
+      </Provider>
+    );
+    const usernameInput = tree.getByTestId("username");
+    fireEvent.changeText(usernameInput, "he");
+    fireEvent(usernameInput, "blur");
+    const errorElement = tree.getByTestId("errorText");
+
+    expect(errorElement.props.children).toEqual(
+      "Username must be more than 4 characters"
+    );
+  });
+
+  it("shows an error message when username characters are only digits", () => {
+    const tree = render(
+      <Provider store={store}>
+        <SignUp />
+      </Provider>
+    );
+    const usernameInput = tree.getByTestId("username");
+    fireEvent.changeText(usernameInput, '12377');
+    fireEvent(usernameInput, "blur");
+    const errorElement = tree.getByTestId("errorText");
+
+    expect(errorElement.props.children).toEqual(
+      "Username cannot be only digits"
+    );
+  });
+
+  it("shows no error component when username has correct characters", () => {
+    const tree = render(
+      <Provider store={store}>
+        <SignUp />
+      </Provider>
+    );
+
+    const tree2 = render(
+      <Provider store={store}>
+        <Input />
+      </Provider>
+    );
+
+    const emailInput = tree.getByTestId("username");
+    fireEvent.changeText(emailInput, "henry");
+    fireEvent(emailInput, "blur");
+    const errorElement = tree2.getByTestId("errorText");
+    expect(errorElement.props.children).toEqual(undefined);
+  });
+
   it("shows an error message when password characters are less", () => {
     const tree = render(
       <Provider store={store}>
-        <SignIn />
+        <SignUp />
       </Provider>
     );
     const passwordInput = tree.getByTestId("password");
@@ -96,7 +162,7 @@ describe("<SignIn />", () => {
   it("shows no error component when password has correct characters", () => {
     const tree = render(
       <Provider store={store}>
-        <SignIn />
+        <SignUp />
       </Provider>
     );
 
@@ -116,11 +182,11 @@ describe("<SignIn />", () => {
   xit("shows an error message when all inputs fields are empty", () => {
     const tree = render(
       <Provider store={store}>
-        <SignIn />
+        <SignUp />
       </Provider>
     );
-    const signinBtn = tree.getByText("Sign In");
-    fireEvent.press(signinBtn);
+    const SignUpBtn = tree.getByText("Sign Up");
+    fireEvent.press(SignUpBtn);
     const errorElement = tree.getAllByTestId("errorText");
     errorElement.map((element) => {
       
@@ -131,17 +197,16 @@ describe("<SignIn />", () => {
   });
 
   xit("shows that the loginHandler is called", () => {
-    
     const tree = render(
       <Provider store={store}>
-        <SignIn />
+        <SignUp />
       </Provider>
     );
-    const loginHandlerFn = jest.spyOn(SignIn, "loginHandler").mockImplementation();
-    const signinBtn = tree.getByText("Sign In");
+    const loginHandlerFn = jest.spyOn(SignUp, "signupHandler").mockImplementation();
+    const SignUpBtn = tree.getByText("Sign Up");
 
     console.log(loginHandlerFn)
-    fireEvent.press(signinBtn);
+    fireEvent.press(SignUpBtn);
     // expect(btn).toHaveBeenCalled();
   });
 });
