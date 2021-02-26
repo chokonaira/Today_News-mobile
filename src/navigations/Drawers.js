42962dd6b0822f74d5122b90e724f92a122358f1
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2021-02-27 01:48:58 +0200

    styled navigation drawer as per screen dimension

--- src/navigations/Drawers.js
+++ src/navigations/Drawers.js
@@ -1,12 +1,19 @@
 import * as React from "react";
+import { useWindowDimensions } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { TodayNewsStacks, ProfileStacks, FaqsStacks } from "./Stacks";
 
 const Drawer = createDrawerNavigator();
 
 export function Drawers() {
+  const dimensions = useWindowDimensions();
+
+  const isLargeScreen = dimensions.width >= 768;
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      drawerType={isLargeScreen ? "permanent" : "slide"}
+      drawerStyle={isLargeScreen ? null : { width: "55%" }}
+    >
       <Drawer.Screen name="Latest News" component={TodayNewsStacks} />
       <Drawer.Screen name="Profile" component={ProfileStacks} />
       <Drawer.Screen name="Faqs" component={FaqsStacks} />
