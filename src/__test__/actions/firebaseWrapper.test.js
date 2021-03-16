02ada5fa592086f597a251340d25c034a6f64d55
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2021-03-16 16:41:49 +0200

    adds test to check that all news belonging to user was fetch from firestore

--- src/__test__/actions/firebaseWrapper.test.js
+++ src/__test__/actions/firebaseWrapper.test.js
@@ -1,8 +1,7 @@
 import firebase from "firebase";
 import {
-  FirestoreWrapper
+  FirestoreWrapper,
 } from '../../redux/actions/FirestoreWrapper'
-import favorites from "../../redux/reducers/favorite";
 
 
 const favoritedArticle = [{
@@ -11,16 +10,38 @@ const favoritedArticle = [{
   publishedAt: '2021-01-01'
 }]
 
+const docsMock = jest.fn(() => {
+  return {
+    doc: jest.fn(() => {
+      return {
+        data: jest.fn().mockResolvedValueOnce(),
+      }
+    })
+  };
+});
+
+const snapshotMock = jest.fn(() => {
+  return {
+    onSnapshot: jest.fn().mockImplementation(()=>  Promise.resolve(favoritedArticle)),
+    ref: jest.fn(() => {
+      return {
+        delete: jest.fn().mockResolvedValueOnce(),
+      }
+    })
+  };
+});
+
 const getMock = jest.fn(() => {
   return {
     get: jest.fn().mockResolvedValueOnce(),
   };
 })
+
 const whereMock = jest.fn(() => {
   return {
     where: whereMock,
     get: getMock,
-    onSnapshot: jest.fn(() => mockReturnThis()),
+    onSnapshot: snapshotMock
   };
 })
 
@@ -30,6 +51,8 @@ const firestoreMock = {
   set: jest.fn().mockResolvedValueOnce(),
   where: whereMock,
   get: getMock,
+  onSnapshot: snapshotMock,
+  docs: docsMock
 };
 
 jest.spyOn(firebase, "firestore").mockImplementation(() => firestoreMock);
@@ -55,9 +78,18 @@ describe('Firestore', () => {
     expect(firestoreMock.where).toHaveBeenNthCalledWith(2, "url", "==", article.url);
     expect(firestoreMock.where).toHaveBeenNthCalledWith(3, "publishedAt", "==", article.publishedAt);
     expect(firestoreMock.get).toHaveBeenCalled();
-    // expect(firestoreMock.get).toBeCalledWith(favoritedArticle);
-    // expect(whereMock.get).toBeCalledWith(favoritedArticle);
+    // expect(firestoreMock.onSnapshot).toHaveBeenCalled();
+    done()
+  });
+
+  it("succesfully query and fectch all favorited article for a user", (done) => {
+    const wrapper = new FirestoreWrapper()
+    wrapper.fetchAllFavorite(article.userEmail)
   
+    expect(firestoreMock.collection).toBeCalledWith("favorites");
+    expect(firestoreMock.where).toHaveBeenNthCalledWith(1, "userEmail", "==", article.userEmail);
+    expect(firestoreMock.get).toHaveBeenCalled();
+    // expect(firestoreMock.docs).toHaveBeenCalled();
     done()
   });
 })
